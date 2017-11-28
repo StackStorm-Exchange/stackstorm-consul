730a4e32badf45a031e52a6400103b5c5996dd4a
Tomaz Muraus <tomaz@tomaz.me>
2017-11-28 10:49:48 +0100

    Fix lint build fix.

--- actions/catalog_node.py
+++ actions/catalog_node.py
@@ -2,20 +2,9 @@ from lib import action
 
 
 class ConsulCatalogNodeAction(action.ConsulBaseAction):
-    def run(
-        self,
-        node,
-        index=None,
-        wait=None,
-        consistency=None,
-        dc=None,
-        token=None
-    ):
-        return (True, self.consul.catalog.node(
-            node,
-            index=index,
-            wait=wait,
-            consistency=consistency,
-            dc=dc,
-            token=token
-            ))
+    def run(self, node, index=None, wait=None, consistency=None, dc=None,
+            token=None):
+        node = self.consul.catalog.node(node, index=index, wait=wait,
+                                        consistency=consistency, dc=dc,
+                                        token=token)
+        return (True, node)
