69d2fc389694cf4a92fe411d1960f60a0a4c06bc
Carlos <nzlosh@yahoo.com>
2017-11-28 09:24:50 +0100

    Aligned Agent Service files with Consul API.

--- actions/agent_service_maintenance.py
+++ actions/agent_service_maintenance.py
@@ -3,7 +3,7 @@ from lib import action
 
 class ConsulServiceMaintenanceAction(action.ConsulBaseAction):
     def run(self, service_id, enable, reason=None):
-        return (True, self.consul.service.maintenance(
+        return (True, self.consul.agent.service.maintenance(
             service_id=service_id,
             enable=enable,
             reason=reason
